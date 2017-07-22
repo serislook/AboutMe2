bbb47995e01aa83060b521cc8fab5f1f39764215
OSX <osx@MBP-de-OSX.home>
2017-07-22 10:54:42 -0400

    Initial commit

---
+++ app/src/main/java/com/octavio/aboutme/ContacActivity.java
@@ -0,0 +1,13 @@
+package com.octavio.aboutme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContacActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contac);
+    }
+}
---
+++ app/src/main/java/com/octavio/aboutme/EmailFragment.java
@@ -0,0 +1,43 @@
+package com.octavio.aboutme;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class EmailFragment extends Fragment {
+
+
+    public EmailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_email, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button contactBtn = (Button) view.findViewById(R.id.contactBtn);
+        contactBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "funciono", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
